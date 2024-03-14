571725e32ec99c91355eb8c95b865a108602d504
Stanislav Chiknavaryan <stanisc@google.com>
2024-03-14 18:07:39 +0000

    main.rs changes to launch the LedgerActor
    Change-Id: Ibe349944f471e0f53dd0923c2e690523e834474c

--- apps/ledger/app/src/main.rs
+++ apps/ledger/app/src/main.rs
@@ -16,11 +16,34 @@
 #![no_main]
 #![feature(alloc_error_handler)]
 
-use oak_restricted_kernel_interface::syscall;
-use oak_restricted_kernel_sdk::{entrypoint, utils::log};
+extern crate alloc;
+extern crate tcp_proto;
+extern crate tcp_runtime;
+
+use alloc::boxed::Box;
+use oak_restricted_kernel_sdk::{
+    channel::{start_blocking_server, FileDescriptorChannel},
+    entrypoint,
+    utils::{log, samplestore::StaticSampleStore},
+};
+use tcp_ledger_service::actor::LedgerActor;
+use tcp_proto::runtime::endpoint::EndpointServiceServer;
+use tcp_runtime::service::ApplicationService;
 
 #[entrypoint]
 fn run_server() -> ! {
     log::info!("In main!");
-    syscall::exit(-1);
+    // Only log warnings and errors to reduce the risk of accidentally leaking execution
+    // information through debug logs.
+    log::set_max_level(log::LevelFilter::Warn);
+
+    let mut invocation_stats = StaticSampleStore::<1000>::new().unwrap();
+    let service: ApplicationService<LedgerActor> = ApplicationService::new(LedgerActor::new());
+    let server = EndpointServiceServer::new(service);
+    start_blocking_server(
+        Box::<FileDescriptorChannel>::default(),
+        server,
+        &mut invocation_stats,
+    )
+    .expect("Server encountered an unrecoverable error");
 }
