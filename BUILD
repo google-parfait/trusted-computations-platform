e2abaa631bd8a354c6a267d22fbcfb2ec1ca61f0
Dzmitry Huba <huba@google.com>
2024-08-23 16:03:46 +0000

    Add bazel build script
    THe script can be used to build and test binaries manually
    or from CI.
    
    Change-Id: Ifebfed8b9d1bc01ac5ac105336e9d88f6ecb5cc4

--- BUILD
+++ BUILD
@@ -11,3 +11,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+load("@rules_pkg//pkg:install.bzl", "pkg_install")
+load("@rules_pkg//pkg:mappings.bzl", "pkg_files")
+
+# All artifacts that will be built for release, along with their names in the
+# destination directory.
+_ALL_BINARIES = {
+    "//apps/atomic_counter/app:tcp_atomic_counter_enclave_app": "tcp_atomic_counter_enclave_app/binary",
+    "//apps/ledger/app:tcp_ledger_enclave_app": "tcp_ledger_enclave_app/binary",
+    "//apps/tablet_cache/app:tcp_tablet_cache_enclave_app": "tcp_tablet_cache_enclave_app/binary",
+    "//apps/tablet_store/app:tcp_tablet_store_enclave_app": "tcp_tablet_store_enclave_app/binary",
+}
+
+pkg_files(
+    name = "all_binaries",
+    srcs = _ALL_BINARIES.keys(),
+    renames = _ALL_BINARIES,
+)
+
+pkg_install(
+    name = "install_binaries",
+    srcs = [":all_binaries"],
+)
---
+++ scripts/build_bazel.sh
@@ -0,0 +1,45 @@
+# Copyright 2024 The Trusted Computations Platform Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# A script that runs the build process for the entire repository.
+# This script can be run locally or in CI.
+#
+# If `release` or `debug` is passed, this indicates that the script should build the
+# artifacts for release or dev. In other words, it will build binaries in the form in
+# which they can be executed in an enclave and export them to
+# BINARY_OUTPUTS_DIR.
+
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# If bazelisk isn't in user's path, the BAZELISK environment variable may be set
+# instead. This may also be used to pass startup options like --nosystem_rc to
+# bazel; this usage requires us to not quote ${BAZELISK} when used later.
+readonly BAZELISK="${BAZELISK:-bazelisk}"
+
+${BAZELISK} test //...
+
+if [ "$1" == "release" ] || [ "$1" == "debug" ]; then
+  mode=$([ "$1" == "release" ] && echo "-c opt" || echo "")
+  # BINARY_OUTPUTS_DIR may be unset if this script is run manually; it'll
+  # always be set during CI builds.
+  if [[ -n "${BINARY_OUTPUTS_DIR}" ]]; then
+    ${BAZELISK} run $mode //:install_binaries -- --destdir "${BINARY_OUTPUTS_DIR}"
+  else
+    # If unset, verify the binaries can be built with -c opt.
+    ${BAZELISK} build $mode //:install_binaries
+  fi
+fi
