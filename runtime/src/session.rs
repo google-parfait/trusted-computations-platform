d673f3fa3f3b98ddc41c8ef5b4e7089738820ca7
Rakshita Tandon <rakshitatandon@google.com>
2024-08-28 20:32:24 +0000

    Add an implementation for UnorderedChannelEncryptor.
    This change is heavily adopted from the default oak crypter
    implementation (https://github.com/project-oak/oak/blob/main/oak_crypto/src/noise_handshake/mod.rs#L75)
    with the following tweaks:
    1. Nonce is also passed along in addition to the encrypted ciphertext.
    2. The transported nonce is used for decryption, thereby enabling
       decryption for re-ordered messages.
    3. Additional test coverage for both ordered and unordered messages.
    
    Change-Id: I9cc7220d48cc4526640bdf4aeeca482f503979f6

--- runtime/src/session.rs
+++ runtime/src/session.rs
@@ -14,7 +14,12 @@
 
 use alloc::boxed::Box;
 use alloc::vec::Vec;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use oak_crypto::encryptor::{Encryptor, Payload};
+use oak_crypto::noise_handshake::{
+    aes_256_gcm_open_in_place, aes_256_gcm_seal_in_place, Nonce, NONCE_LEN, SYMMETRIC_KEY_LEN,
+};
+use oak_proto_rust::oak::crypto::v1::SessionKeys;
 use oak_proto_rust::oak::session::v1::{
     session_request::Request, session_response::Response, SessionRequest, SessionResponse,
 };
@@ -141,6 +146,10 @@ impl DefaultOakServerSession {
         Ok(Self {
             _inner: ServerSession::new(
                 SessionConfig::builder(AttestationType::Bidirectional, HandshakeType::NoiseNN)
+                    .set_encryption_provider(Box::new(|sk| {
+                        <SessionKeys as TryInto<UnorderedChannelEncryptor>>::try_into(sk)
+                            .map(|v| Box::new(v) as Box<dyn Encryptor>)
+                    }))
                     .build(),
             ),
             incoming_ciphertext: None,
@@ -183,5 +192,168 @@ impl OakSession<SessionRequest, SessionResponse> for DefaultOakServerSession {
 
     fn read(&mut self) -> Result<Option<Vec<u8>>> {
         Ok(self.incoming_ciphertext.take())
+    }
+}
+
+// Custom encryptor to use with the Noise protocol capable of handling
+// unordered and dropped messages.
+// TODO: Move to oak repo once the implementation is stable and
+// verified.
+pub struct UnorderedChannelEncryptor {
+    read_key: [u8; SYMMETRIC_KEY_LEN],
+    write_key: [u8; SYMMETRIC_KEY_LEN],
+    write_nonce: Nonce,
+}
+
+impl UnorderedChannelEncryptor {
+    fn new(read_key: &[u8; SYMMETRIC_KEY_LEN], write_key: &[u8; SYMMETRIC_KEY_LEN]) -> Self {
+        Self {
+            read_key: *read_key,
+            write_key: *write_key,
+            write_nonce: Nonce { nonce: 0 },
+        }
+    }
+}
+impl Encryptor for UnorderedChannelEncryptor {
+    fn encrypt(&mut self, payload: Payload) -> Result<Payload> {
+        const PADDING_GRANULARITY: usize = 32;
+        let plaintext = payload.message.as_slice();
+
+        let mut padded_size: usize = plaintext.len();
+        // AES GCM is limited to encrypting 64GiB of data in a single AEAD invocation.
+        // 256MiB is just a sane upper limit on message size, which greatly exceeds
+        // the noise specified 64KiB, which will be too restrictive for our use cases.
+        if padded_size > (1usize << 28) {
+            return Err(anyhow!(
+                "Data exceeds max allowed size 256MiB, Actual Size: {:?},",
+                padded_size
+            ));
+        }
+        padded_size += 1; // padding-length byte
+
+        // This is standard low-level bit manipulation to round up to the nearest
+        // multiple of PADDING_GRANULARITY.  We know PADDING_GRANULARRITY is a
+        // power of 2, so we compute the mask with !(PADDING_GRANULARITY - 1).
+        // If padded_size is not already a multiple of PADDING_GRANULARITY, then
+        // padded_size will not change.  Otherwise, it is rounded up to the next
+        // multiple of PADDED_GRANULARITY.
+        padded_size = (padded_size + PADDING_GRANULARITY - 1) & !(PADDING_GRANULARITY - 1);
+
+        let mut padded_encrypt_data = Vec::with_capacity(padded_size);
+        padded_encrypt_data.extend_from_slice(plaintext);
+        padded_encrypt_data.resize(padded_size, 0u8);
+        let num_zeros = padded_size - plaintext.len() - 1;
+        padded_encrypt_data[padded_size - 1] = num_zeros as u8;
+
+        let next_nonce = &self
+            .write_nonce
+            .next()
+            .map_err(|e| anyhow!("Failed to get nonce error: {e:#?}"))?;
+        aes_256_gcm_seal_in_place(&self.write_key, next_nonce, &[], &mut padded_encrypt_data);
+
+        Ok(Payload {
+            message: padded_encrypt_data,
+            nonce: Some(next_nonce.to_vec()),
+            aad: None,
+        })
+    }
+
+    fn decrypt(&mut self, payload: Payload) -> Result<Payload> {
+        let ciphertext = payload.message.as_slice();
+        let nonce: [u8; NONCE_LEN] = payload
+            .nonce
+            .unwrap()
+            .try_into()
+            .map_err(|e| anyhow!("Failed to extract nonce error: {e:#?}"))?;
+        let plaintext =
+            aes_256_gcm_open_in_place(&self.read_key, &nonce, &[], Vec::from(ciphertext))
+                .map_err(|()| anyhow!("Failed to decrypt message."))?;
+
+        // Plaintext must have a padding byte, and the unpadded length must be
+        // at least one.
+        if plaintext.is_empty() || (plaintext[plaintext.len() - 1] as usize) >= plaintext.len() {
+            return Err(anyhow!("Decryption padding failed."));
+        }
+        let unpadded_length = plaintext.len() - (plaintext[plaintext.len() - 1] as usize);
+        Ok(Payload {
+            message: Vec::from(&plaintext[0..unpadded_length - 1]),
+            nonce: None,
+            aad: None,
+        })
+    }
+}
+
+impl TryFrom<SessionKeys> for UnorderedChannelEncryptor {
+    type Error = anyhow::Error;
+
+    fn try_from(sk: SessionKeys) -> Result<Self, Self::Error> {
+        Ok(UnorderedChannelEncryptor::new(
+            sk.response_key
+                .as_slice()
+                .try_into()
+                .map_err(|e| anyhow!("Unexpected format of the read key: {e:#?}"))?,
+            sk.request_key
+                .as_slice()
+                .try_into()
+                .map_err(|e| anyhow!("Unexpected format of the read key: {e:#?}"))?,
+        ))
+    }
+}
+
+#[cfg(all(test, feature = "std"))]
+mod test {
+    use oak_crypto::encryptor::Encryptor;
+    use oak_crypto::{encryptor::Payload, noise_handshake::SYMMETRIC_KEY_LEN};
+
+    use crate::session::UnorderedChannelEncryptor;
+
+    #[test]
+    fn test_encryption_decryption_ordered() {
+        let key_1 = &[42u8; SYMMETRIC_KEY_LEN];
+        let key_2 = &[52u8; SYMMETRIC_KEY_LEN];
+        let test_messages = vec![vec![1u8, 2u8, 3u8, 4u8], vec![4u8, 3u8, 2u8, 1u8], vec![]];
+        let mut replica_1 = UnorderedChannelEncryptor::new(key_1, key_2);
+        let mut replica_2 = UnorderedChannelEncryptor::new(key_2, key_1);
+
+        for message in &test_messages {
+            let payload = Payload {
+                message: message.to_vec(),
+                nonce: None,
+                aad: None,
+            };
+            let encrypted_payload = replica_1.encrypt(payload).unwrap();
+            let plaintext = replica_2.decrypt(encrypted_payload).unwrap().message;
+            assert_eq!(message, &plaintext);
+        }
+    }
+
+    #[test]
+    fn test_encryption_decryption_unordered() {
+        let key_1 = &[42u8; SYMMETRIC_KEY_LEN];
+        let key_2 = &[52u8; SYMMETRIC_KEY_LEN];
+        let test_messages = vec![vec![1u8, 2u8, 3u8, 4u8], vec![4u8, 3u8, 2u8, 1u8]];
+        let mut replica_1 = UnorderedChannelEncryptor::new(key_1, key_2);
+        let mut replica_2 = UnorderedChannelEncryptor::new(key_2, key_1);
+
+        let encrypted_payload_1 = replica_1
+            .encrypt(Payload {
+                message: test_messages[0].to_vec(),
+                nonce: None,
+                aad: None,
+            })
+            .unwrap();
+        let encrypted_payload_2 = replica_1
+            .encrypt(Payload {
+                message: test_messages[1].to_vec(),
+                nonce: None,
+                aad: None,
+            })
+            .unwrap();
+
+        // Decrypt in reverse order
+        let plaintext_2 = replica_2.decrypt(encrypted_payload_2).unwrap().message;
+        let plaintext_1 = replica_2.decrypt(encrypted_payload_1).unwrap().message;
+        assert_eq!(test_messages[0], plaintext_1);
+        assert_eq!(test_messages[1], plaintext_2);
     }
 }
